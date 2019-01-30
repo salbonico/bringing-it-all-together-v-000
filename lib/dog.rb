d2094fea60e0373d5067bcf60c38cb4423969f07
Steve Albonico <salbonico@gmail.com>
2019-01-30 00:37:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -60,7 +60,7 @@ SQL
 returned_dog = DB[:conn].execute(sql,name,breed)
 
 if !returned_dog.empty?
-  return Dog.new(returned_dog[0],returned_dog[1],returned_dog[2])
+  return Dog.new({id:returned_dog[0],name:returned_dog[1],breed:returned_dog[2]})
 else
   self.create({name:name, breed:breed})
 end
